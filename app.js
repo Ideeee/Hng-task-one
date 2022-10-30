2638cc11a20f620238953b3b0b242df5c98878f6
Idara-Cecilia Umana <idyumana.iu@gmail.com>
2022-10-30 22:59:03 +0100

    Fixed syntax

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 let express = require('express');
 let app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 
 app.get("/", async (req,res)=>{
     res.json({
         'slackUsername' : 'Idee',
         "backend" : true,
-        'age' : 81,
+        'age' : 21,
         'bio': "Hi, It's nice to meet you"
     })
 })
